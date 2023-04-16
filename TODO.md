f8e0a34ed84acfaaf90df6f93c75a99f76f4d224
Yiqin Zhao <yiqinzhao@outlook.com>
2023-04-16 16:15:03 -0400

    :sparkles: Removed BFM data files.

---
+++ TODO.md
@@ -0,0 +1,12 @@
+# TODO
+
+This repo is forked from a third party implementation of Basel face fitting with PyTorch.
+Although the code functions well, the project is not well written nor maintained.
+Consider refactor the code to improve efficiency and code qualities.
+
+- [ ] Upgrade PyTorch and PyTorch3D to newer version for better performance.
+- [ ] Rename Python files to follow the naming convention.
+- [ ] Remove the `gifs` folder.
+- [ ] Fix the video fitting part.
+
+Once every todo items were done, merge the code into our main repo, discard the submodule.
--- core/__init__.py
+++ core/__init__.py
@@ -2,10 +2,14 @@ from scipy.io import loadmat
 
 from third_party.face_fitting_pytorch.core.BFM09Model import BFM09ReconModel
 
+DEFAULT_BFM09_MODEL_PATH = (
+    "/research-data/yiqinzhao/InterLight/basel-face-model/BFM09_model_info.mat"
+)
+
 
 def get_recon_model(model="bfm09", **kargs):
     if model == "bfm09":
-        model_path = "third_party/face_fitting_pytorch/BFM/BFM09_model_info.mat"
+        model_path = DEFAULT_BFM09_MODEL_PATH
         model_dict = loadmat(model_path)
         recon_model = BFM09ReconModel(model_dict, **kargs)
         return recon_model
